634e42d6f2ce8ada1504174e2d4209e7b6d12bfd
mzolimpieff <mzolimpieff@gmail.com>
2018-08-09 18:19:06 -0300

    init browse report

---
+++ src/browse_report/get-browse-report.js
@@ -0,0 +1,7 @@
+module.exports = class GetBrowseReport {
+    constructor() {}
+
+    get() {
+
+    }
+}
---
+++ test/get-browse-report-test.js
@@ -0,0 +1,5 @@
+var BrowseReport = require('./../src/browse_report/get-browse-report')
+var test = require('unit.js')
+
+var browseReport = new BrowseReport()
+browseReport.get()
