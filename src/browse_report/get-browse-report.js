db40d462564bf2a19345f904899ffb690dfd8c6a
mzolimpieff <mzolimpieff@gmail.com>
2018-08-10 10:00:24 -0300

    browse report structure and tests

--- src/browse_report/get-browse-report.js
+++ src/browse_report/get-browse-report.js
@@ -1,7 +1,42 @@
 module.exports = class GetBrowseReport {
-    constructor() {}
+    constructor() {
+    }
+    
+    export() {
+        return {
+            "browser" : this.getBrowser(),
+            "isCookiesAllowed" : this.getIsCookiesAllowed(),
+            "browserWidth" : this.getWidth(),
+            "browserheight" : this.getHeight(),
+            "screenWidth" : this.getScreenWidth(),
+            "screenHeight" : this.getScreenWidth()
+        }
+    }
+    getBrowser() {
+        return navigator.userAgent
+    }
+    getIsCookiesAllowed() {
+        return (document.cookie.indexOf(document.cookie) >= 0)
+    }
 
-    get() {
+    getWidth() {
+        return window.innerWidth || document.documentElement.width
+    }
+
+    getHeight() {
+        return window.innerHeight
+    }
+
+    getScreenWidth() {
+        return screen.width
+    }
+
+    getScreenHeight() {
+        return screen.height
+    }
+
+    getUserIp() {
 
     }
+
 }
--- test/get-browse-report-test.js
+++ test/get-browse-report-test.js
@@ -2,4 +2,37 @@ var BrowseReport = require('./../src/browse_report/get-browse-report')
 var test = require('unit.js')
 
 var browseReport = new BrowseReport()
-browseReport.get()
+
+navigator = {
+    "userAgent" : "bla"
+}
+document = {
+    "documentElement" : {
+        "width" : "100",
+        "height" : "100"
+    }
+}
+window = {
+    "innerHeight" : "0",
+    "innerWidth" : "0"
+}
+
+screen =  {
+    height : "120",
+    width : "120"
+}
+
+test.string("bla").is(browseReport.getBrowser())
+test.string("0").is(browseReport.getWidth())
+test.string("0").is(browseReport.getWidth())
+test.string("0").is(browseReport.getHeight())
+test.string("120").is(browseReport.getScreenWidth())
+test.string("120").is(browseReport.getScreenHeight())
+
+window.innerWidth = "100"
+test.string("100").is(browseReport.getWidth())
+
+screen.height = "100"
+test.string("100").is(browseReport.getScreenHeight())
+
+
